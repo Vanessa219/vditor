91d59d496d6cd587e629952373303edede443be2
Van <lly219@gmail.com>
2019-03-14 18:43:39 +0800

    :white_check_mark: add commonmark test

---
+++ __test__/markdown/markdown-it-render.test.ts
@@ -0,0 +1,19 @@
+import MarkdownIt from 'markdown-it'
+import {spec} from './commonmark-0.28'
+
+it('MarkdownIt', () => {
+    let right = 0;
+    let error = 0;
+    spec.forEach(async (item: any) => {
+        const result = new MarkdownIt({
+            xhtmlOut: true
+        }).render(item.markdown)
+        if (result === item.html) {
+            right++
+        } else {
+            error++
+        }
+        expect(result).toBe(item.html)
+    })
+    console.log(`pass:${right}, error:${error}`)
+})
