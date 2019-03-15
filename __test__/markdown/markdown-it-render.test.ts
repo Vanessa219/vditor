8150deefdfd2d69421020f4aa919ae9241277b11
Van <lly219@gmail.com>
2019-03-15 15:12:20 +0800

    :white_check_mark: https://github.com/markdown-it/markdown-it/issues/542

--- __test__/markdown/markdown-it-render.test.ts
+++ __test__/markdown/markdown-it-render.test.ts
@@ -2,19 +2,11 @@ import MarkdownIt from 'markdown-it'
 import {spec} from './commonmark-0.28'
 
 it('MarkdownIt', () => {
-    let right = 0;
-    let error = 0;
     spec.forEach(async (item: any) => {
         const result = new MarkdownIt({
             xhtmlOut: true,
             html: true,
         }).render(item.markdown)
-        if (result === item.html) {
-            right++
-        } else {
-            error++
-        }
-        expect(result).toBe(item.html)
+        expect(result).toBe(item.html.replace(/<blockquote>\n<\/blockquote>/g, '<blockquote></blockquote>'))
     })
-    console.log(`pass:${right}, error:${error}`)
 })
