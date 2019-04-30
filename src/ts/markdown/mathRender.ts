30ecfa7d44174130cf44ee7897d9a78bc9d6ddd8
Van <lly219@gmail.com>
2019-04-30 16:48:23 +0800

    :rotating_light:

--- src/ts/markdown/mathRender.ts
+++ src/ts/markdown/mathRender.ts
@@ -7,7 +7,8 @@ export const mathRender = (element: HTMLElement) => {
         import(/* webpackChunkName: "katex" */ "katex").then(() => {
             import(/* webpackChunkName: "katex" */ "katex/contrib/auto-render/auto-render")
                 .then((renderMathInElement) => {
-                    addStyle(`${CDN_PATH}/vditor@${VDITOR_VERSION}/dist/js/katex@0.10.1/katex.min.css`, "vditorKatexStyle");
+                    addStyle(`${CDN_PATH}/vditor@${VDITOR_VERSION}/dist/js/katex@0.10.1/katex.min.css`,
+                        "vditorKatexStyle");
                     renderMathInElement.default(element, {
                         delimiters: [
                             {left: "$$", right: "$$", display: true},
