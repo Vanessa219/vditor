c05f1fd67fcb1a79b119d534b663566845070183
Van <lly219@gmail.com>
2019-06-07 13:49:23 +0800

    :at: process render bug #58

--- src/ts/markdown/chartRender.ts
+++ src/ts/markdown/chartRender.ts
@@ -3,8 +3,17 @@ export const chartRender = async (element: (HTMLElement | Document) = document) 
     if (echartsElements.length > 0) {
         const {default: echarts} = await import(/* webpackChunkName: "echarts" */ "echarts");
         echartsElements.forEach((e: HTMLDivElement) => {
-            const option = JSON.parse(e.innerHTML);
-            echarts.init(e).setOption(option);
+            try {
+                if (e.getAttribute('data-processed') === 'true') {
+                    return
+                }
+                const option = JSON.parse(e.innerHTML.trim());
+                echarts.init(e).setOption(option);
+                e.setAttribute('data-processed', 'true')
+            } catch (error) {
+                e.className = 'hljs'
+                e.innerHTML = `echarts render error: <br>${error}`;
+            }
         });
     }
 };
