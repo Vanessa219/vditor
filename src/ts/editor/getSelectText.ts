ab1c2199f24dbe71d9375df38c626cc0549e568d
Van <lly219@gmail.com>
2019-08-05 23:01:22 +0800

    :bug: fix #87

--- src/ts/editor/getSelectText.ts
+++ src/ts/editor/getSelectText.ts
@@ -5,7 +5,9 @@ export const getSelectText = (range: Range, editor: HTMLDivElement) => {
         // window.getSelection().toString() 无法获取最后一个 br
         const element = document.createElement("div");
         element.appendChild(range.cloneContents());
-        return element.innerHTML.replace(/<br>/g, "\n").replace(/&nbsp;/g, " ");
+        return element.innerHTML.replace(/<br>/g, "\n").replace(/&nbsp;/g, " ")
+            .replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+            .replace(/&amp;/g, '&');
     }
 
     return "";
