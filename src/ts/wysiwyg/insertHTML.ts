9469ee1df40eadbae4d995bccc81807607b0c11f
Van <lly219@gmail.com>
2020-01-02 21:08:47 +0800

    :bug:

--- src/ts/wysiwyg/insertHTML.ts
+++ src/ts/wysiwyg/insertHTML.ts
@@ -7,7 +7,7 @@ export const insertHTML = (html: string, vditor: IVditor) => {
     const range = getSelection().getRangeAt(0);
     if (!range.collapsed) {
         vditor.wysiwyg.preventInput = true;
-        document.execCommand("delete", false, "r");
+        document.execCommand("delete", false, "");
 
     }
     range.insertNode(pasteElement.content.cloneNode(true));
