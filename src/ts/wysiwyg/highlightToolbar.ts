cd5dac76e68e81450a2084c134955dea415698cb
Van <lly219@gmail.com>
2020-02-20 13:16:19 +0800

    :bug: fix #142

--- src/ts/wysiwyg/highlightToolbar.ts
+++ src/ts/wysiwyg/highlightToolbar.ts
@@ -134,7 +134,7 @@ export const highlightToolbar = (vditor: IVditor) => {
                     range.insertNode(document.createElement("wbr"));
                     const parentTagName = liElement.parentElement.tagName;
                     liElement.previousElementSibling.insertAdjacentHTML("beforeend",
-                        `<${parentTagName} data-block="0"><li data-marker="${liElement.getAttribute("data-marker").slice(-1)}">${liElement.innerHTML}</li></${parentTagName}>`);
+                        `<${parentTagName} data-block="0"><li data-marker="1${liElement.getAttribute("data-marker").slice(-1)}">${liElement.innerHTML}</li></${parentTagName}>`);
                     liElement.remove();
 
                     // D 说需要调用 2 次，方可没有 p
