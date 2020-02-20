945d253596d990a160939d1f7c4906be8a96bcdc
Van <lly219@gmail.com>
2020-02-20 14:09:32 +0800

    :bug: #156 1

--- src/ts/wysiwyg/setHeading.ts
+++ src/ts/wysiwyg/setHeading.ts
@@ -3,22 +3,30 @@ import {setRangeByWbr} from "./setRangeByWbr";
 
 export const setHeading = (vditor: IVditor, tagName: string) => {
     const range = getSelection().getRangeAt(0);
-    range.insertNode(document.createElement("wbr"));
-
-    const blockElement = hasClosestBlock(range.startContainer);
-    if (blockElement) {
-        blockElement.outerHTML = `<${tagName} data-block="0">${blockElement.innerHTML}</${tagName}>`;
+    let blockElement = hasClosestBlock(range.startContainer);
+    if (!blockElement) {
+        blockElement = range.startContainer.childNodes[range.startOffset] as HTMLElement;
     }
-    setRangeByWbr(vditor.wysiwyg.element, range);
+    if (blockElement) {
+        range.insertNode(document.createElement("wbr"));
+        if (blockElement.tagName === "BLOCKQUOTE") {
+            blockElement.innerHTML = `<${tagName} data-block="0">${blockElement.innerHTML}</${tagName}>`;
+        } else {
+            blockElement.outerHTML = `<${tagName} data-block="0">${blockElement.innerHTML}</${tagName}>`;
+        }
+        setRangeByWbr(vditor.wysiwyg.element, range);
+    }
 };
 
 export const removeHeading = (vditor: IVditor) => {
     const range = getSelection().getRangeAt(0);
-    range.insertNode(document.createElement("wbr"));
-
-    const blockElement = hasClosestBlock(range.startContainer);
-    if (blockElement) {
-        blockElement.outerHTML = `<p data-block="0">${blockElement.innerHTML}</p>`;
+    let blockElement = hasClosestBlock(range.startContainer);
+    if (!blockElement) {
+        blockElement = range.startContainer.childNodes[range.startOffset] as HTMLElement;
     }
-    setRangeByWbr(vditor.wysiwyg.element, range);
+    if (blockElement) {
+        range.insertNode(document.createElement("wbr"));
+        blockElement.outerHTML = `<p data-block="0">${blockElement.innerHTML}</p>`;
+        setRangeByWbr(vditor.wysiwyg.element, range);
+    }
 };
