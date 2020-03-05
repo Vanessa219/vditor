5df46a6ba11fb7ca1f84224b6c6142e844386908
Van <lly219@gmail.com>
2020-03-05 20:48:14 +0800

    :bug: worker cross domain

--- src/ts/markdown/graphvizRender.ts
+++ src/ts/markdown/graphvizRender.ts
@@ -4,7 +4,7 @@ import {addScript} from "../util/addScript";
 declare class Viz {
     public renderSVGElement: (code: string) => Promise<any>;
 
-    constructor({}: { workerURL: string });
+    constructor({}: { worker: Worker });
 }
 
 export const graphvizRender = (element: HTMLElement, cdn = `https://cdn.jsdelivr.net/npm/vditor@${VDITOR_VERSION}`) => {
@@ -19,13 +19,24 @@ export const graphvizRender = (element: HTMLElement, cdn = `https://cdn.jsdelivr
         if (e.getAttribute("data-processed") === "true") {
             return;
         }
-        new Viz({workerURL: "../../js/graphviz/full.render.js"})
-            .renderSVGElement(e.textContent).then((result: HTMLElement) => {
-            e.innerHTML = result.outerHTML;
-        }).catch((error) => {
-            e.innerHTML = `graphviz render error: <br>${error}`;
-            e.className = "vditor-math vditor-reset--error";
-        });
+
+        try {
+            const blob = new Blob([`importScripts('${cdn}/dist/js/graphviz/full.render.js');`],
+                {type: "application/javascript"});
+            const url = window.URL || window.webkitURL;
+            const blobUrl = url.createObjectURL(blob);
+            const worker = new Worker(blobUrl);
+            new Viz({worker})
+                .renderSVGElement(e.textContent).then((result: HTMLElement) => {
+                e.innerHTML = result.outerHTML;
+            }).catch((error) => {
+                e.innerHTML = `graphviz render error: <br>${error}`;
+                e.className = "vditor-math vditor-reset--error";
+            });
+        } catch (e) {
+            console.error("graphviz error", e);
+        }
+
         e.setAttribute("data-processed", "true");
     });
 };
