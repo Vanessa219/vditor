80967acbd076b26118c49a3834da38ce18835244
Liyuan Li <v@b3log.org>
2020-04-24 11:58:32 +0800

    :pencil: info

--- src/ts/toolbar/Info.ts
+++ src/ts/toolbar/Info.ts
@@ -1,4 +1,4 @@
-import { VDITOR_VERSION} from "../constants";
+import {VDITOR_VERSION} from "../constants";
 import {getEventName} from "../util/compatibility";
 import {MenuItem} from "./MenuItem";
 
@@ -7,29 +7,35 @@ export class Info extends MenuItem {
         super(vditor, menuItem);
         this.element.children[0].addEventListener(getEventName(), (event) => {
             event.preventDefault();
-            vditor.tip.show(`<div style="max-width: 520px; font-size: 14px;line-height: 22px;margin: 0 0 7px 6px;">
+            vditor.tip.show(`<div style="max-width: 520px; font-size: 14px;line-height: 22px;margin-bottom: 14px;">
 <p style="text-align: center">
-<img src="https://cdn.jsdelivr.net/npm/vditor/src/assets/images/logo.png"><br>
-<em>下一代的 Markdown 编辑器，为未来而构建</em>
+    <em>下一代的 Markdown 编辑器，为未来而构建</em>
 </p>
-<p>
-Vditor 是一款浏览器端的 Markdown 编辑器，支持所见即所得、即时渲染（类似 Typora）和分屏预览模式。
-它使用 TypeScript 实现，支持原生 JavaScript、Vue、React 和Angular。</p>
+<div style="display: flex;margin-bottom: 14px">
+    <img src="https://cdn.jsdelivr.net/npm/vditor/src/assets/images/logo.png" style="margin-right: 10px;height: 68px"/>
+    <div>
+        Vditor 是一款浏览器端的 Markdown 编辑器，支持所见即所得、即时渲染（类似 Typora）和分屏预览模式。
+        它使用 TypeScript 实现，支持原生 JavaScript、Vue、React 和Angular。
+    </div>
+</div>
 <div style="display: flex;align-items: center">
-<ul style="flex: 1;list-style: none">
-    <li>
-    项目地址：<a href="https://github.com/Vanessa219/vditor" target="_blank">https://github.com/Vanessa219/vditor</a>
-    </li>
-    <li>
-    开源协议：MIT
-    </li>
-    <li>
-    组件版本：Vditor v${VDITOR_VERSION} / Lute v${Lute.Version}
-    </li>
-    <li>
-    赞助捐赠：<a href="https://hacpai.com/sponsor" target="_blank">https://hacpai.com/sponsor</a>
-    </li>
-</ul>
+    <ul style="list-style: none;flex: 1">
+        <li>
+        项目地址：<a href="https://github.com/Vanessa219/vditor" target="_blank">GitHub</a>
+        <a href="https://gitee.com/dl88250/vditor" target="_blank">码云</a>
+        </li>
+        <li>
+        开源协议：MIT
+        </li>
+    </ul>
+    <ul style="list-style: none;margin-right: 18px">
+        <li>
+        组件版本：Vditor v${VDITOR_VERSION} / Lute v${Lute.Version}
+        </li>
+        <li>
+        赞助捐赠：<a href="https://hacpai.com/sponsor" target="_blank">https://hacpai.com/sponsor</a>
+        </li>
+    </ul>
 </div>
 </div>`, 0);
         });
