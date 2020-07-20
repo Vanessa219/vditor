36b1422fa415531631a94516eb42beeab1974ed1
Liyuan Li <v@b3log.org>
2020-07-20 11:03:15 +0800

    :art: #598

--- demo/index.js
+++ demo/index.js
@@ -79,11 +79,14 @@ window.vditor = new Vditor('vditor', {
       'j': 'https://unpkg.com/vditor@1.3.1/dist/images/emoji/j.png',
     },
     at: (key) => {
-      return [
-        {
-          value: '@Vanessa',
-          html: '<img src="https://avatars0.githubusercontent.com/u/970828?s=60&v=4"/> Vanessa',
-        }]
+      if ('vanessa'.indexOf(key.toLocaleLowerCase()) > -1) {
+        return [
+          {
+            value: '@Vanessa',
+            html: '<img src="https://avatars0.githubusercontent.com/u/970828?s=60&v=4"/> Vanessa',
+          }]
+      }
+      return []
     },
   },
   tab: '\t',
