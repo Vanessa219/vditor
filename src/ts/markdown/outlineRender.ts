877b037d7a962b285ecf1ec9f812074d59698846
Vanessa <lly219@gmail.com>
2020-11-27 12:18:17 +0800

    :sparkles: fix #830

--- src/ts/markdown/outlineRender.ts
+++ src/ts/markdown/outlineRender.ts
@@ -1,59 +1,61 @@
 import {hasClosestByHeadings} from "../util/hasClosestByHeadings";
+import {mathRender} from "./mathRender";
 
 export const outlineRender = (contentElement: HTMLElement, targetElement: Element, vditor?: IVditor) => {
     let tocHTML = "";
-    Array.from(contentElement.children).forEach((item: HTMLElement, index) => {
+    const ids: string[] = []
+    Array.from(contentElement.children).forEach((item: HTMLElement, index: number) => {
         if (hasClosestByHeadings(item)) {
-            const headingNo = parseInt(item.tagName.substring(1), 10);
-            const space = new Array((headingNo - 1) * 2).fill("&emsp;").join("");
-            let text = "";
-            if (vditor && vditor.currentMode === "ir") {
-                const markerElement = item.querySelector('[data-type="heading-marker"]');
-                if (markerElement.getAttribute("data-render") === "2") {
-                    text = item.textContent.replace(markerElement.textContent, "").trim();
-                } else {
-                    text = item.textContent.substring(headingNo + 1).trim();
-                }
-            } else {
-                text = item.textContent.trim();
-            }
             const lastIndex = item.id.lastIndexOf("_");
-            const lastId = item.id.substring(0, lastIndex === -1 ? undefined : lastIndex);
-            item.id = lastId + "_" + index;
-            tocHTML += `<div data-id="${item.id}" class="vditor-outline__item">${space}${text}</div>`;
+            item.id = item.id.substring(0, lastIndex === -1 ? undefined : lastIndex) + "_" + index;
+            ids.push(item.id);
+            tocHTML += item.outerHTML.replace("<wbr>", "");
         }
     });
-    targetElement.innerHTML = tocHTML;
-    targetElement.querySelectorAll(".vditor-outline__item").forEach((item) => {
-        item.addEventListener("click", (event: Event & { target: HTMLElement }) => {
-            const idElement = document.getElementById(item.getAttribute("data-id"));
-            if (!idElement) {
-                return;
-            }
-            if (vditor) {
-                if (vditor.options.height === "auto") {
-                    let windowScrollY = idElement.offsetTop + vditor.element.offsetTop;
-                    if (!vditor.options.toolbarConfig.pin) {
-                        windowScrollY += vditor.toolbar.element.offsetHeight;
-                    }
-                    window.scrollTo(window.scrollX, windowScrollY);
-                } else {
-                    if (vditor.element.offsetTop < window.scrollY) {
-                        window.scrollTo(window.scrollX, vditor.element.offsetTop);
-                    }
-                    if (vditor.preview.element.contains(contentElement)) {
-                        contentElement.parentElement.scrollTop = idElement.offsetTop;
-                    } else {
-                        contentElement.scrollTop = idElement.offsetTop;
-                    }
-                }
-            } else {
-                window.scrollTo(window.scrollX, idElement.offsetTop);
-            }
-            targetElement.querySelectorAll(".vditor-outline__item").forEach((subItem) => {
-                subItem.classList.remove("vditor-outline__item--current");
-            });
-            item.classList.add("vditor-outline__item--current");
+    if (tocHTML !== "") {
+        const tempElement = document.createElement("div");
+        if (vditor.currentMode === "wysiwyg") {
+            tempElement.innerHTML = vditor.lute.SpinVditorDOM("<p>[ToC]</p>" + tocHTML);
+        } else if (vditor.currentMode === "ir") {
+            tempElement.innerHTML = vditor.lute.SpinVditorIRDOM("<p>[ToC]</p>" + tocHTML);
+        }
+        tempElement.firstElementChild.querySelectorAll("li > span[data-target-id]").forEach((item, index) => {
+            item.setAttribute("data-target-id", ids[index]);
         });
-    });
+        tocHTML = tempElement.firstElementChild.innerHTML;
+        targetElement.innerHTML = tocHTML;
+        mathRender(targetElement as HTMLElement, {
+            cdn: vditor.options.cdn,
+            math: vditor.options.preview.math,
+        });
+        targetElement.querySelectorAll("li > span").forEach((item) => {
+            item.addEventListener("click", (event: Event & { target: HTMLElement }) => {
+                const idElement = document.getElementById(item.getAttribute("data-target-id"));
+                if (!idElement) {
+                    return;
+                }
+                if (vditor) {
+                    if (vditor.options.height === "auto") {
+                        let windowScrollY = idElement.offsetTop + vditor.element.offsetTop;
+                        if (!vditor.options.toolbarConfig.pin) {
+                            windowScrollY += vditor.toolbar.element.offsetHeight;
+                        }
+                        window.scrollTo(window.scrollX, windowScrollY);
+                    } else {
+                        if (vditor.element.offsetTop < window.scrollY) {
+                            window.scrollTo(window.scrollX, vditor.element.offsetTop);
+                        }
+                        if (vditor.preview.element.contains(contentElement)) {
+                            contentElement.parentElement.scrollTop = idElement.offsetTop;
+                        } else {
+                            contentElement.scrollTop = idElement.offsetTop;
+                        }
+                    }
+                } else {
+                    window.scrollTo(window.scrollX, idElement.offsetTop);
+                }
+            });
+        });
+    }
+    return tocHTML;
 };
